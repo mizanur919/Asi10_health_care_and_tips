1fb1800af7ab25231146db8376eba2afbf2d54b7
mizan <mizanur919@gmail.com>
2021-10-18 21:23:58 +0600

    spinner added

--- src/App.js
+++ src/App.js
@@ -6,30 +6,34 @@ import Home from './Pages/Home/Home/Home'
 import NotFound from './Pages/NotFound/NotFound'
 import Login from './Pages/Login/Login';
 import ServiceDetails from './Pages/ServiceDetails/ServiceDetails';
+import AuthProvider from './contexts/AuthProvider';
+import PrivateRoute from './Pages/Login/PrivateRoute/PrivateRoute';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Header></Header>
-        <Switch>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-          <Route path="/home">
-            <Home></Home>
-          </Route>
-          <Route path="/login">
-            <Login></Login>
-          </Route>
-          <Route path="/servicedetails/:serviceId">
-            <ServiceDetails></ServiceDetails>
-          </Route>
-          <Route path="*">
-            <NotFound></NotFound>
-          </Route>
-        </Switch>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Header></Header>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route path="/home">
+              <Home></Home>
+            </Route>
+            <Route path="/login">
+              <Login></Login>
+            </Route>
+            <PrivateRoute path="/servicedetails/:serviceId">
+              <ServiceDetails></ServiceDetails>
+            </PrivateRoute>
+            <Route path="*">
+              <NotFound></NotFound>
+            </Route>
+          </Switch>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useLocation, useHistory } from 'react-router-dom';
+import useAuth from '../../contexts/useAuth';
 
 
 const Login = () => {
+    const {signInUsingGoogle, isLoading, setIsLoading} = useAuth();
+    const location = useLocation();
+    const history = useHistory();
+    const redirect_uri = location.state?.from || "/home";
+
+    const handleLogIn = () =>{
+        setIsLoading(true)
+        signInUsingGoogle()
+        .then(result =>{
+            history.push(redirect_uri);
+        })
+        .finally(() => setIsLoading(false));
+    }
     return (
         <div className="login-container d-flex justify-content-center align-items-center">
             <div className="bg-white p-4 rounded col-md-3 mx-auto align-items-center">
-                <h4 className="font-weight-bold mb-4">Therapy Care Point</h4>
+                <h4 className="font-weight-bold mb-4 text-uppercase">Therapy Care Point</h4>
                 <Form>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Control type="email" placeholder="Enter email" />
@@ -21,7 +35,7 @@ const Login = () => {
                 <NavLink className="new-account" as={Link} to="/signup">New to Cafe Spice? Create Account.</NavLink>
                 <hr />
                 <p>Or</p>
-                <button className="google-signing-button btn btn-primary btn-sm" type="submit">Google</button>
+                <button onClick={handleLogIn} className="google-signing-button btn btn-primary btn-sm" type="submit">Google</button>
             </div>
         </div>
     );
