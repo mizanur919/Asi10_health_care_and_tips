e7fb06fb351a7710355a744f0b178d15484a8c12
mizan <mizanur919@gmail.com>
2021-10-19 23:52:44 +0600

    sticky menu

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -8,38 +8,37 @@ import './Header.css'
 const Header = () => {
     const { user, logOut } = useAuth();
     return (
-        <div>
-            <div className="header-section">
-                <Navbar className="header-nav" variant="light" sticky="top">
-                    <Container>
-                        <Nav.Link>
-                            <Navbar.Brand as={HashLink} to="/home#home" className="brand"><span className="text-white text-uppercase fw-bold">Therapy <span className="text-info">Care</span> Point</span></Navbar.Brand>
-                        </Nav.Link>
+        <>
+            <Navbar className="header-nav" variant="light" sticky="top">
+                <Container>
+                    <Nav.Link>
+                        <Navbar.Brand as={HashLink} to="/home" className="brand"><span className="text-white text-uppercase fw-bold">Therapy <span className="text-info">Care</span> Point</span></Navbar.Brand>
+                    </Nav.Link>
 
-                        <Nav className="justify-content-end my-nav">
-                            <Nav.Link as={HashLink} to="/home#home"><span className="text-white">Home</span></Nav.Link>
-                            <Nav.Link as={HashLink} to="/home#services"><span className="text-white">Services</span></Nav.Link>
-                            <Nav.Link as={HashLink} to="/talktoexperts"><span className="text-white">Talk To Specialist</span></Nav.Link>
-                            <Nav.Link as={HashLink} to="/aboutus"><span className="text-white">About Us</span></Nav.Link>
-                            {
-                                user.email
-                                    ?
-                                    <div>
-                                        <Button onClick={logOut} variant="info"><span className="text-white">LogOut</span></Button>
-                                        <span> Hello, {user.displayName} </span>
-                                        <img style={{ width: '40px', height: '40px', borderRadius: '50%' }} src={user.photoURL} alt="" />
-                                    </div>
-                                    :
-                                    <div className="d-flex">
-                                        <Nav.Link as={Link} to="/login"><span className="text-white">Login</span></Nav.Link>
-                                        <Nav.Link as={Link} className="signup-button" to="/signup"><span className="text-white">Sign up</span></Nav.Link>
-                                    </div>
-                            }
-                        </Nav>
-                    </Container>
-                </Navbar>
-            </div>
-        </div>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        <Nav.Link as={HashLink} to="/home"><span className="text-white">Home</span></Nav.Link>
+                        <Nav.Link as={HashLink} to="/home#services"><span className="text-white">Services</span></Nav.Link>
+                        <Nav.Link as={HashLink} to="/talktoexperts"><span className="text-white">Talk To Specialist</span></Nav.Link>
+                        <Nav.Link as={HashLink} to="/aboutus"><span className="text-white">About Us</span></Nav.Link>
+                        {
+                            user.email
+                                ?
+                                <div>
+                                    <Button onClick={logOut} variant="info"><span className="text-white">LogOut</span></Button>
+                                    <span> Hello, {user.displayName} </span>
+                                    <img style={{ width: '40px', height: '40px', borderRadius: '50%' }} src={user.photoURL} alt="" />
+                                </div>
+                                :
+                                <div className="d-flex">
+                                    <Nav.Link as={Link} to="/login"><span className="text-white">Login</span></Nav.Link>
+                                    <Nav.Link as={Link} className="signup-button" to="/signup"><span className="text-white">Sign up</span></Nav.Link>
+                                </div>
+                        }
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     );
 };
 
